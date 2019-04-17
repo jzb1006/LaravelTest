445b333a47bed628f2c631523c6020da2fb7da1e
jzb1006 <1129990545@qq.com>
2019-04-17 10:34:37 +0800

    微博数据模型

--- app/Models/User.php
+++ app/Models/User.php
@@ -38,6 +38,11 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    //一对多，这里关联的是status（动态）的模型表，一个用户可以有多个动态
+    public function statuses(){
+        return $this->hasMany(Status::class);
+    }
+
     public function gravatar($size = 100){
         $hash = md5(strtolower(trim($this->attributes['email'])));
         return "http://www.gravatar.com/avatar/$hash?s=$size";
