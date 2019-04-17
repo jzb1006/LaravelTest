ff7f520fc70dadde6644c608b637d46fa177afe2
jzb1006 <1129990545@qq.com>
2019-04-17 14:41:44 +0800

    微博删除

--- resources/views/users/_user.blade.php
+++ resources/views/users/_user.blade.php
@@ -2,7 +2,7 @@
     <img src="{{ $user->gravatar() }}" alt="{{ $user->name }}" class="gravatar">
     <a href="{{ route('users.show', $user->id )}}" class="username">{{ $user->name }}</a>
     @can('destroy', $user)
-        <form action="{{ route('users.destroy', $user->id) }}" method="post">
+        <form action="{{ route('users.destroy', $user->id) }}" method="post" onsubmit="return confirm('您确定要删除吗？');">
             {{ csrf_field() }}
             {{ method_field('DELETE') }}
             <button type="submit" class="btn btn-sm btn-danger delete-btn">删除</button>
