d3a6abf6278995dedf510691dbd844642c5c4934
jzb1006 <1129990545@qq.com>
2019-04-17 14:07:38 +0800

    发布微博

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Status extends Model
 {
+    protected $fillable = ['content'];
     //多对一
     public function user(){
         return $this->belongsTo(User::class);
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -2,16 +2,31 @@
 @section('title','主页')
 
 @section('content')
-    <div class="jumbotron">
-        <h1>Hello Laravel</h1>
-        <p class="lead">
-            你现在所看到的是 <a href="https://laravel-china.org/courses/laravel-essential-tra"></a>
-        </p>
-<p>
-一切，将从这里开始。
-</p>
-<p>
-<a class="btn btn-lg btn-success" href="{{route('signup')}}" role="button">现在注册</a>
-        </p>
-    </div>
+    @if(\Illuminate\Support\Facades\Auth::check())
+        <div class="row">
+            <div class="col-md-8">
+                <section class="status_form">
+                    @include('shared._status_form')
+                </section>
+            </div>
+            <aside class="col-md-4">
+                <section class="user_info">
+                    @include('shared._user_info',['user'=>\Illuminate\Support\Facades\Auth::user()]);
+                </section>
+            </aside>
+        </div>
+    @else
+        <div class="jumbotron">
+            <h1>Hello Laravel</h1>
+            <p class="lead">
+                你现在所看到的是 <a href="https://laravel-china.org/courses/laravel-essential-tra"></a>
+            </p>
+            <p>
+                一切，将从这里开始。
+            </p>
+            <p>
+                <a class="btn btn-lg btn-success" href="{{route('signup')}}" role="button">现在注册</a>
+            </p>
+        </div>
+    @endif
 @stop
